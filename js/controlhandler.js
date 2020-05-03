86fe1a62c953a7bb0ef79a57301795e62cb686ed
David <dlchaves81@gmail.com>
2020-05-02 22:04:45 -0300

    asd
    asdasd

--- js/controlhandler.js
+++ js/controlhandler.js
@@ -1,6 +1,7 @@
 var result;
 function loadcontrol(controlurl)
 {
+  $("#content").find("div").remove();
     $.get( controlurl, function( data ) {
         $( "#content" ).append( $(data) );
       },"text");
